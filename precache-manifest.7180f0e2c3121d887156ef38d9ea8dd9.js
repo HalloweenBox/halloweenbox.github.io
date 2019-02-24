b70a26ffbb91a9b016d79a717367b1df8dd40455
Halloween Box <halloweenbox@outlook.com>
2019-02-24 17:10:19 +0200

    Updates

---
+++ precache-manifest.7180f0e2c3121d887156ef38d9ea8dd9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d56e34a8bd07e4bf900b44164fa005a",
+    "url": "/static/media/scary.2d56e34a.png"
+  },
+  {
+    "revision": "18cce0ee0617fc535d74",
+    "url": "/static/css/main.d560d7c4.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "09009ec0bae90d3bc47f",
+    "url": "/static/js/2.09009ec0.chunk.js"
+  },
+  {
+    "revision": "72f144586ba78f81f41194e57e6c92b3",
+    "url": "/static/media/dpthStory.72f14458.png"
+  },
+  {
+    "revision": "6680a749b858533ed820f24b9f10c547",
+    "url": "/static/media/happy.6680a749.png"
+  },
+  {
+    "revision": "18cce0ee0617fc535d74",
+    "url": "/static/js/main.18cce0ee.chunk.js"
+  },
+  {
+    "revision": "fb9e83ffe9e16ff19d5fe7c3aca86692",
+    "url": "/static/media/halloween-showcase-small.fb9e83ff.jpg"
+  },
+  {
+    "revision": "de667fe7d7fd30a2de2cc35966c45cd7",
+    "url": "/static/media/halloween-showcase-large.de667fe7.jpg"
+  },
+  {
+    "revision": "7ccbe3a20d7dbd2155152bfd737da95d",
+    "url": "/static/media/LACETRIM.7ccbe3a2.woff2"
+  },
+  {
+    "revision": "7fb43bb09261289fed8965acb39e3b1f",
+    "url": "/static/media/LACETRIM.7fb43bb0.woff"
+  },
+  {
+    "revision": "2b3f149ada903ae93340bbb69c648678",
+    "url": "/static/media/LACETRIM.2b3f149a.ttf"
+  },
+  {
+    "revision": "dd44efdccbf9540e005c45c159a9fd73",
+    "url": "/index.html"
+  }
+];
